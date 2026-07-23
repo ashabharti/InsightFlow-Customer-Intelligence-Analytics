f27bb43dafb923e672cc2d106dca9f7f0291d5d5
ashabharti <bhartiasha44@gmail.com>
2026-07-24 02:38:31 +0530

    Create products table

--- database/02_create_tables.sql
+++ database/02_create_tables.sql
@@ -48,3 +48,19 @@ CREATE TABLE order_items (
 
     PRIMARY KEY (order_id, order_item_id)
 );
+
+-- ==========================================
+-- Products
+-- ==========================================
+
+CREATE TABLE IF NOT EXISTS products (
+    product_id VARCHAR(32) PRIMARY KEY,
+    product_category_name VARCHAR(100),
+    product_name_length INTEGER,
+    product_description_length INTEGER,
+    product_photos_qty INTEGER,
+    product_weight_g INTEGER,
+    product_length_cm INTEGER,
+    product_height_cm INTEGER,
+    product_width_cm INTEGER
+);
