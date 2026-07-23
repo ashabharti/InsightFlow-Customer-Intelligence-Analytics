01d5e8f2b00073dd8942d1d00d35ee65be0bd436
ashabharti <bhartiasha44@gmail.com>
2026-07-24 02:05:55 +0530

    Add database project structure

---
+++ database/01_create_database.sql
@@ -0,0 +1,8 @@
+ -- ==========================================
+-- Project : InsightFlow Customer Intelligence Analytics
+-- File    : 01_create_database.sql
+-- Author  : Asha Bharti
+-- Purpose : Create the project database
+-- ==========================================
+
+CREATE DATABASE insightflow_db;
