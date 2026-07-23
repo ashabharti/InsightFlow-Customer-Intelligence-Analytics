b9f2e7b21181dade368b483ac5c8e476c076707f
ashabharti <bhartiasha44@gmail.com>
2026-07-24 02:01:34 +0530

    Add orders table schema

--- database/02_create_tables.sql
+++ database/02_create_tables.sql
@@ -15,3 +15,19 @@ CREATE TABLE customers (
     customer_city VARCHAR(100) NOT NULL,
     customer_state CHAR(2) NOT NULL
 );
+
+-- ==========================================
+-- Table: orders
+-- Purpose: Stores customer order information
+-- ==========================================
+
+CREATE TABLE orders (
+    order_id VARCHAR(32) PRIMARY KEY,
+    customer_id VARCHAR(32) NOT NULL,
+    order_status VARCHAR(20) NOT NULL,
+    order_purchase_timestamp TIMESTAMP NOT NULL,
+    order_approved_at TIMESTAMP,
+    order_delivered_carrier_date TIMESTAMP,
+    order_delivered_customer_date TIMESTAMP,
+    order_estimated_delivery_date TIMESTAMP NOT NULL
+);
